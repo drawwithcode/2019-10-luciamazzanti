c7e57b8473be400e8211ccb4562b32d999129a5e
Lucia Mazanti <luciamazzanti4@gmail.com>
2020-01-04 17:48:39 +0100

    es 10

--- sketch.js
+++ sketch.js
@@ -1,11 +1,23 @@
-function preload(){
-  // put preload code here
-}
-
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  wolf = loadModel("assets/wolf.obj", true);
+  angleMode(DEGREES);
 }
 
 function draw() {
-  // put drawing code here
+  background(10);
+
+  var locX = mouseX - height / 2;
+  var locY = mouseY - width / 2;
+
+  ambientLight(0, 10, 50);
+  directionalLight(255, 0, 10, 0, 1, 0);
+  pointLight(0, 255, 255, locX, locY, 0);
+
+  noStroke(255);
+
+  rotateZ(180);
+  rotateY(frameCount * 0.9);
+
+  model(wolf);
 }
